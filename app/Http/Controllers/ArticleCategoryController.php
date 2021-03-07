7e8bba3e92c840327cbf802f07bc45ecc67b45af
spexhu <radoslav.kuchar@gmail.com>
2021-03-07 16:00:58 +0100

    1.3.7

--- app/Http/Controllers/ArticleCategoryController.php
+++ app/Http/Controllers/ArticleCategoryController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ArticleCategory;
 
 class ArticleCategoryController extends Controller
 {
-    //
+    public function create(){
+        return view('articlecategory.create');
+    }
+    public function store(Request $request){
+        $data = $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        ArticleCategory::create([
+            'name' => $data['name'],
+        ]);
+    }
 }
